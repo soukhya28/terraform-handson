78470114e31ba7d4a8f594ff5a258fa1616ebfaa
soukhya28 <soukhya468@gmail.com>
2025-07-17 11:26:04 +0000

    last-commit

--- resource.tf
+++ resource.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "terrahandson" {
     name     = "terrahandson-resources"
     location = "East US"
 }
+resource "azurerm_resource_group" "terrahandsonexercise" {
+    name     = "terrahandsonexercise-resources"
+    location = "East US"
+}
