a4dd04375cfc486cdc7cb1acfbd5838c2a428452
soukhya28 <soukhya468@gmail.com>
2025-07-17 11:37:47 +0000

    latest-commit

--- resource.tf
+++ resource.tf
@@ -6,3 +6,15 @@ resource "azurerm_resource_group" "terrahandsonexercise" {
     name     = "terrahandsonexercise-resources"
     location = "East US"
 }
+resource "azurerm_storage_account" "soterra" {
+    name                     = "terrastoragesored286"
+    resource_group_name      = azurerm_resource_group.terrahandson.name
+    location                 = azurerm_resource_group.terrahandson.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+resource "azurerm_storage_container" "socontainer" {
+    name                  = "terrahandson-container"
+    storage_account_name  = azurerm_storage_account.soterra.name
+    container_access_type = "private"
+}
